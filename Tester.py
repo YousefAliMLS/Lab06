048832214cdf087ba0b442f401ff9fdcb086762b
Yousef-Ali <YousefMahmoudAli@outlook.com>
2025-11-02 15:39:50 +0200

    First commiting

---
+++ Tester.py
@@ -0,0 +1,7 @@
+import test
+
+class Testing:
+    O = test.Task()
+    print(O.add( 5, 10))
+    print(O.subtract( 10, 5))
+    print(O.print(123))
---
+++ test.py
@@ -0,0 +1,9 @@
+class Task:
+    def subtract(self, x , y):
+        return x  - y;
+    def add(self, x , y):
+        return x + y
+    def print(self, z ):
+        if z == 123:
+            return "yes"
+        
